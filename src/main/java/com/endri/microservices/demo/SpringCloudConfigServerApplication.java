ee7e98076afbaf60ee7f1fbdfb0fa4e7f25a1398
endri <endri.zylali@gmail.com>
2018-10-23 16:55:04 +0200

    Spring Cloud configurations

---
+++ src/main/java/com/endri/microservices/demo/SpringCloudConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.endri.microservices.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+    }
+}
